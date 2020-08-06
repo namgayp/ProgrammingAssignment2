84041ca4cf9ce936fe11939f07ff1714962eab66
namgayp <namgaypema@hotmail.com>
2020-08-06 13:01:19 +0600

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this functionf
 
 
 ##The first function, makeCacheMatrix creates and cache the inverse matrix, cntaining the following 
